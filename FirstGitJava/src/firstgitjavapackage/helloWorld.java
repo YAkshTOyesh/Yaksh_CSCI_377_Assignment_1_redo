a6e3e40e50549ea8337e6254993e288c24d7dd28
Yaksh Ujoodha <yakshujoodha@gmail.com>
2024-09-13 21:34:04 -0500

    Added years in Beloit in java program

--- FirstGitJava/src/firstgitjavapackage/helloWorld.java
+++ FirstGitJava/src/firstgitjavapackage/helloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
 	        // Prints "Hello, World" in the terminal window.
 	        System.out.println("Hello, World");
+                System.out.println("I have been in Beloit for more than 3 years");
 }
 
